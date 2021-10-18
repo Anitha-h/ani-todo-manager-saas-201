c754d7e695863ba614b1b2076ac08b543160b9f0
Anitha-h <anihcet@gmail.com>
2021-10-18 21:42:51 +0530

    Add ability to remove any todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,4 @@
 class Todo < ActiveRecord::Base
-  # def to_pleasant_string
-  #  is_completed = completed ? "[ X ]" : "[ ]"
-  #  "#{id}.  #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
-  # end
-
   def due_today?
     due_date == Date.due_today
   end
@@ -20,21 +15,6 @@ class Todo < ActiveRecord::Base
 
   def self.completed
     where(completed: true)
-  end
-
-  def self.show_list
-    puts "My Todo-list\n\n"
-    puts "Overdue\n"
-    puts overdue.show_list
-    puts "\n\n"
-
-    puts "Due_Today\n"
-    puts due_today.show_list
-    puts "\n\n"
-
-    puts "Due Later\n"
-    puts due_later.show_list
-    puts "\n\n"
   end
 
   def self.mark_as_complete!(id)
