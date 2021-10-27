669924d950d45b1d323e7a33a77f5e7ceae0eaea
Anitha-h <anihcet@gmail.com>
2021-10-27 21:32:48 +0530

    user can now completed and update an existing record

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,40 +1,26 @@
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
-
   def index
-    #render plain:"Hello to Rails! #{DateTime.now.to_s(:short)}"
-    #render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
-
   def show
     id = params[:id]
     todo = Todo.find(id)
-    #render plain:"The id you entered was #{id}"
-    #render plain: todo.to_pleasant_string
-    #render "todo"
   end
-
   def create
-    #render plain:"Hey this a Create action"
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    #response_text = "Hey your new todo is created with the  id #{new_todo.id}"
-    #render plain: response_text
     redirect_to todos_path
   end
-
   def update
     id = params[:id]
     completed = params[:completed]
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    # render plain: "Updated todo completed status to #{completed}"
     redirect_to todos_path
   end
-
   def destroy
     id = params[:id]
     todo = Todo.find(id)
