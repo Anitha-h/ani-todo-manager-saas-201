729a81826e105f61dd05269dc98ba6fc42f4e551
Anitha-h <anihcet@gmail.com>
2021-11-15 22:36:11 +0530

    Added signing and updating session

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,22 @@
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
     render "index"
   end
+
   def show
     id = params[:id]
     todo = Todo.find(id)
   end
+
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     redirect_to todos_path
   end
+
   def update
     id = params[:id]
     completed = params[:completed]
@@ -21,6 +25,7 @@ class TodosController < ApplicationController
     todo.save!
     redirect_to todos_path
   end
+
   def destroy
     id = params[:id]
     todo = Todo.find(id)
