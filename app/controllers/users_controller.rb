ee8e0ed2090aa800a9c4efe3ca1330028bad9532
Anitha-h <anihcet@gmail.com>
2021-11-18 21:41:50 +0530

    Validtaes user first name and email before signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
@@ -14,6 +14,11 @@ class UsersController < ApplicationController
 
     )
 
-    redirect_to "/"
+    if new_user.save
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to "/"
+    end
   end
 end
